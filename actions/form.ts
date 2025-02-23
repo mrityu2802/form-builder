2588b886bb14304deb3687afa01eae0364a7cf6c
mrityu2802 <mmrityunjay727@gmail.com>
2025-02-23 11:43:37 +0530

    form actions

--- actions/form.ts
+++ actions/form.ts
@@ -3,7 +3,7 @@
 import { formSchema, FormSchema } from "@/schema/form";
 import { revalidatePath } from "next/cache";
 import { PrismaClient } from "@prisma/client";
-
+import { currentUser } from "@clerk/nextjs/server";
 const prisma = new PrismaClient();
 
 export const createForm = async (data: FormSchema) => {
@@ -11,7 +11,7 @@ export const createForm = async (data: FormSchema) => {
   if (!validatedFields.success) {
     throw new Error("Invalid fields");
   }
-  const user = "mrityunjay";
+  const user = await currentUser();
   if (!user) {
     throw new Error("User not found");
   }
@@ -19,7 +19,7 @@ export const createForm = async (data: FormSchema) => {
     data: {
       name: data.name,
       description: data.description,
-      userId: user,
+      userId: user.id,
     },
   });
   if (!form) {
@@ -30,17 +30,179 @@ export const createForm = async (data: FormSchema) => {
 };
 
 export const GetForms = async () => {
-  const user = "mrityunjay";
+  const user = await currentUser();
   if (!user) {
     throw new Error("User not found");
   }
   const forms = await prisma.form.findMany({
     where: {
-      userId: user,
+      userId: user.id,
     },
     orderBy: {
       createdAt: "desc",
     },
   });
   return forms;
+};
+
+export const getFormById = async (id: string) => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const form = await prisma.form.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+  if (!form) {
+    throw new Error("Form not found");
+  }
+  return form;
+};
+
+export const updateForm = async (id: string, jsonData: string) => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const form = await prisma.form.update({
+    where: {
+      id,
+      userId: user.id,
+    },
+    data: {
+      content: jsonData,
+    },
+  });
+  if (!form) {
+    throw new Error("Form not found");
+  }
+  return form;
+};
+
+export const publishForm = async (id: string) => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const form = await prisma.form.update({
+    where: {
+      id,
+      userId: user.id,
+    },
+    data: {
+      published: true,
+    },
+  });
+  if (!form) {
+    throw new Error("Form not found");
+  }
+  return form;
+};
+
+// export const getFormStats = async (id: string) => {
+//   const user = "mrityunjay";
+//   if (!user) {
+//     throw new Error("User not found");
+//   }
+//   const stats = await prisma.form.aggregate({
+//     where: {
+//       id,
+//       userId: user,
+//     },
+//     _sum: {
+//       views: true,
+//       submissions: true,
+//     },
+//   });
+
+//   const totalViews = stats._sum.views || 0;
+//   const totalSubmissions = stats._sum.submissions || 0;
+
+//   return { totalViews, totalSubmissions };
+// };
+
+export const getSubmissionsByFormId = async (id: string) => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const submissions = await prisma.form.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+    include: {
+      FormSubmission: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
+  return submissions;
+};
+
+export const getFormContent = async (id: string) => {
+  const form = await prisma.form.update({
+    select: {
+      content: true,
+    },
+    data: {
+      views: {
+        increment: 1,
+      },
+    },
+    where: {
+      id,
+    },
+  });
+  return form;
+};
+
+export const submitFormToDatabase = async (id: string, jsonData: string) => {
+  const form = await prisma.form.update({
+    where: {
+      id,
+      published: true,
+    },
+    data: {
+      submissions: {
+        increment: 1,
+      },
+      FormSubmission: {
+        create: {
+          submissionData: jsonData,
+        },
+      },
+    },
+  });
+  if (!form) {
+    throw new Error("Form not found");
+  }
+  return form;
+};
+
+export const deleteForm = async (id: string) => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  // Delete all related form submissions
+  await prisma.formSubmission.deleteMany({
+    where: {
+      formId: id,
+    },
+  });
+
+  // Then delete the form
+  return await prisma.form.delete({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
 };
